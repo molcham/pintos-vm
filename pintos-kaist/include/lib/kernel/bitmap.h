97817a51c69e6fa571cb6fe0de17f0b32e766e2b
leejaewoong <iamlee103@gmail.com>
2025-06-04 01:50:21 +0000

    🧹 chore(bitmap.c, bitmap.h) : bitmap 관련 영문 주석 번역

--- pintos-kaist/include/lib/kernel/bitmap.h
+++ pintos-kaist/include/lib/kernel/bitmap.h
@@ -5,25 +5,25 @@
 #include <stddef.h>
 #include <inttypes.h>
 
-/* Bitmap abstract data type. */
+/* 비트맵을 추상 자료형으로 정의한 헤더입니다. */
 
-/* Creation and destruction. */
+/* 비트맵을 생성하고 파괴하는 함수들 */
 struct bitmap *bitmap_create (size_t bit_cnt);
 struct bitmap *bitmap_create_in_buf (size_t bit_cnt, void *, size_t byte_cnt);
 size_t bitmap_buf_size (size_t bit_cnt);
 void bitmap_destroy (struct bitmap *);
 
-/* Bitmap size. */
+/* 비트맵이 가지는 전체 비트의 개수를 다루는 함수 */
 size_t bitmap_size (const struct bitmap *);
 
-/* Setting and testing single bits. */
+/* 한 비트씩 설정하거나 검사하는 함수들 */
 void bitmap_set (struct bitmap *, size_t idx, bool);
 void bitmap_mark (struct bitmap *, size_t idx);
 void bitmap_reset (struct bitmap *, size_t idx);
 void bitmap_flip (struct bitmap *, size_t idx);
 bool bitmap_test (const struct bitmap *, size_t idx);
 
-/* Setting and testing multiple bits. */
+/* 여러 비트를 한꺼번에 조작할 때 사용하는 함수들 */
 void bitmap_set_all (struct bitmap *, bool);
 void bitmap_set_multiple (struct bitmap *, size_t start, size_t cnt, bool);
 size_t bitmap_count (const struct bitmap *, size_t start, size_t cnt, bool);
@@ -32,12 +32,12 @@ bool bitmap_any (const struct bitmap *, size_t start, size_t cnt);
 bool bitmap_none (const struct bitmap *, size_t start, size_t cnt);
 bool bitmap_all (const struct bitmap *, size_t start, size_t cnt);
 
-/* Finding set or unset bits. */
+/* 원하는 값의 비트를 검색하는 함수 */
 #define BITMAP_ERROR SIZE_MAX
 size_t bitmap_scan (const struct bitmap *, size_t start, size_t cnt, bool);
 size_t bitmap_scan_and_flip (struct bitmap *, size_t start, size_t cnt, bool);
 
-/* File input and output. */
+/* 파일과 비트맵 간의 입출력 지원 */
 #ifdef FILESYS
 struct file;
 size_t bitmap_file_size (const struct bitmap *);
@@ -45,7 +45,7 @@ bool bitmap_read (struct bitmap *, struct file *);
 bool bitmap_write (const struct bitmap *, struct file *);
 #endif
 
-/* Debugging. */
+/* 디버그용 출력 함수 */
 void bitmap_dump (const struct bitmap *);
 
 #endif /* lib/kernel/bitmap.h */
