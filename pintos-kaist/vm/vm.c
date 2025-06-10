a8c8c544878ad436d82333ae4fe964f58836bc45
leejaewoong <iamlee103@gmail.com>
2025-06-10 16:05:35 +0000

    ✨ feat :  merge, inherit tests passed (*)
    merge
    - process_exec : 실행 파일 로드 전에 lock을 획득한 후 진행
    
    inherit
    - supplemental_page_table_copy : file 페이지 초기화 전 page의 aux 필드의 주소를 복사하여 초기화 함수에 전달

--- pintos-kaist/vm/vm.c
+++ pintos-kaist/vm/vm.c
@@ -372,11 +372,7 @@ bool supplemental_page_table_copy(struct supplemental_page_table *dst UNUSED,
 			
 			/* file인 경우, aux 전달 */
 			case VM_FILE:
-				_aux->file = temp_page->file.aux->file;
-				_aux->ofs = temp_page->file.aux->ofs;
-				_aux->page_read_bytes = temp_page->file.aux->page_read_bytes;
-				_aux->page_zero_bytes = temp_page->file.aux->page_zero_bytes;
-
+				_aux = temp_page->file.aux;
 				vm_alloc_page_with_initializer(VM_FILE, temp_page->va, temp_page->writable, lazy_load_segment, _aux);
 				
 				/* spt 등록 후 frame 연결 */
